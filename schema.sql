e07de094d50619d04bf9c224da5e80c12bf2afaf
Yna Mataya <ylmataya@yahoo.com>
2020-04-28 18:48:23 -0500

    created working employee table data

--- schema.sql
+++ schema.sql
@@ -1,3 +1,5 @@
+DROP DATABASE IF EXISTS work_db;
+
 CREATE DATABASE work_db;
 
 USE work_db;
@@ -8,15 +10,41 @@ CREATE TABLE department (
   PRIMARY KEY (id)
 );
 
+INSERT INTO department (names) 
+VALUES("Accounting");
+INSERT INTO department (names) 
+VALUES("Creative");
+INSERT INTO department (names) 
+VALUES("Marketing");
+INSERT INTO department (names) 
+VALUES("IT");
+
 USE work_db;
 
 CREATE TABLE roles (
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL(5,2) NOT NULL,
-  department VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
+  department_id INT(11) NOT NULL,
   PRIMARY KEY (id)
 );
+
+INSERT INTO roles (title, salary, department_id) 
+VALUES("Accountant", 5000, 1);
+INSERT INTO roles (title, salary, department_id) 
+VALUES("Clerk", 3000, 1);
+INSERT INTO roles (title, salary, department_id) 
+VALUES("Designer", 2000, 2);
+INSERT INTO roles (title, salary, department_id) 
+VALUES("UI/UX Researcher", 2500, 2);
+INSERT INTO roles (title, salary, department_id) 
+VALUES("Marketing Consultant", 1500, 3);
+INSERT INTO roles (title, salary, department_id) 
+VALUES("Marketing Manager", 3000, 3);
+INSERT INTO roles (title, salary, department_id) 
+VALUES("Network Admin", 8000, 4);
+INSERT INTO roles (title, salary, department_id) 
+VALUES("Business Analyst", 7000, 4);
 
 USE work_db;
 
@@ -25,7 +53,30 @@ CREATE TABLE employee (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT(11) NOT NULL,
-  manager_id INT(11),
+  manager_id INT(11) NOT NULL,
   PRIMARY KEY (id)
 );
 
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+VALUES("John ", "Gibbs", 5, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+VALUES("Andrea", "Hanson", 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+VALUES("Alex", "Moreno", 6, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+VALUES("Samantha", "Bennett", 4, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+VALUES("Natalia", "Young", 1, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+VALUES("Anthony", "Reyes", 3, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+VALUES("Alyssa", "Mills", 7, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+VALUES("Zack", "Moore", 8, 1);
+
+SELECT * FROM employee LEFT JOIN (roles, department)
+ON (roles.id = employee.role_id AND department.id = roles.department_id);
+
+SELECT * FROM employee LEFT JOIN (roles, department) 
+ON (roles.id = employee.role_id AND department.id = roles.department_id)
+WHERE department.id = 2;
--- work.js
+++ work.js
@@ -1,110 +1,136 @@
-const inquirer = require("inquirer");
-var mysql = require("mysql");
+const inquirer = require('inquirer');
+var mysql = require('mysql');
 const cTable = require('console.table');
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "rootroot",
-  database: "work_db"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "rootroot",
+    database: "work_db"
 });
 
-connection.connect(function(err){
+connection.connect(function (err) {
     if (err) throw err;
     askUser();
-    // connection.end();
 });
 
 function askUser() {
     inquirer
-        .prompt([
-            {
-                type: 'list',
-                message: 'What would you like to do?',
-                name: 'choice',
-                choices: ['View All Employees', 'View All Employees by Department', 'View All Employees by Manager', 'Add Employee', 'Remove Employee', 'Update Employee Role', 'Update Employee Manager', 'View All Roles']
-            }
-        ]).then(function(response) {
+        .prompt([{
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'choice',
+            choices: ['View All Employees', 'View All Roles', 'View by Department', 'Add Employee', 'Update Employee Role', 'Remove Employee', 'Done']
+        }]).then(function (response) {
             if (response.choice === 'View All Employees') {
                 viewEmployees();
-            }
-            else if (response.choice === 'View All Employees by Department') {
+            } else if (response.choice === 'View by Department') {
                 viewByDepartment();
-            }
-            else if (response.choice === 'View All Employees by Manager') {
-                viewByManager();
-            }
-            else if (response.choice === 'Add Employee') {
-                addEmployee();
-            }
-            else if (response.choice === 'Remove Employee') {
-                removeEmployee();
-            }
-            else if (response.choice === 'Update Employee Role') {
-                updateRole();
-            }
-            else if (response.choice === 'Update Employee Manager') {
-                updateManager();
-            }
-            else if (response.choice === 'View All Roles') {
+            } else if (response.choice === 'View All Roles') {
                 viewRoles();
+            } else if (response.choice === 'Add Employee') {
+                addEmployee();
+            } else if (response.choice === 'Remove Employee') {
+                removeEmployee();
+            } else if (response.choice === 'Update Employee Role') {
+                updateRole();
+            } else if (response.choice === 'Done') {
+                connection.end();
+                console.log("Application has ended.")
             }
-            console.log(response); //change it in not a console
         })
 }
 
 function viewEmployees() {
-    // connection.query('', function(err, data){
-    //     if (err) throw err;
-    //     console.log(data);
-    // });
+    connection.query('SELECT * FROM employee LEFT JOIN (roles, department) ON (roles.id = employee.role_id AND department.id = roles.department_id)', function (err, data) {
+        if (err) throw err;
+        console.table(data);
+        askUser();
+    });
 }
 
 function viewByDepartment() {
-    // connection.query('', function(err, data){
-    //     if (err) throw err;
-    //     console.log(data);
-    // });
+    inquirer.prompt([
+        {   type: 'input',
+            message: 'What is the id for your department? 1-Accounting, 2-Creative, 3-Marketing, 4-IT',
+            name: 'id',
+        }  
+    ]).then(data=>{
+        connection.query(`SELECT * FROM employee LEFT JOIN (roles, department) ON (roles.id = employee.role_id AND department.id = roles.department_id) WHERE department.id = ${data.id}`, function (err, data) {
+            if (err) throw err;
+            console.table(data);
+            askUser();
+        });
+    })  
 }
 
-function viewByManager() {
-    // connection.query('', function(err, data){
-    //     if (err) throw err;
-    //     console.log(data);
-    // });
+function viewRoles() {
+    connection.query('SELECT employee.first_name, employee.last_name, roles.title FROM employee, roles WHERE roles.id=employee.role_id', function (err, data) {
+        if (err) throw err;
+        console.table(data);
+        askUser();
+    });
 }
 
 function addEmployee() {
-    // connection.query('', function(err, data){
-    //     if (err) throw err;
-    //     console.log(data);
-    // });
+    inquirer.prompt([
+        {   type: 'input',
+            message: 'What is the FIRST name of the employee?',
+            name: 'first_name',
+        },
+        {   type: 'input',
+            message: 'What is the LAST name of the employee?',
+            name: 'last_name',
+        },
+        {   type: 'input',
+            message: 'What is the role of the employee? 1-Accountant, 2-Clerk, 3-Designer, 4-UI/UX Researcher, 5-Marketing Consultant, 6-Marketing Manager, 7-Network Admin, 8-Business Analyst',
+            name: 'role',
+        }  
+    ]).then(data=>{
+        connection.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES('${data.first_name}', '${data.last_name}', ${data.role}, 1);`, function(err, data){ 
+        if (err) throw err;
+        console.table(data);
+        console.log("Successfully added!")
+        askUser()
+    })
+    })  
 }
 
 function removeEmployee() {
-    // connection.query('', function(err, data){
-    //     if (err) throw err;
-    //     console.log(data);
-    // });
+        inquirer.prompt([
+            {   type: 'input',
+                message: 'What is the LAST name of the employee you want to remove?',
+                name: 'lastName',
+            }  
+        ]).then(data => {
+            connection.query(`DELETE FROM employee WHERE last_name='${data.lastName}'`, function(err, data){
+            if (err) throw err;
+            console.table(data);
+            console.log("Successfully Removed Employee!")
+            askUser()
+        });
+    })
 }
 
 function updateRole() {
-    // connection.query('', function(err, data){
-    //     if (err) throw err;
-    //     console.log(data);
-    // });
-}
-
-function updateManager() {
-    // connection.query('', function(err, data){
-    //     if (err) throw err;
-    //     console.log(data);
-    // });
-}
-function viewRoles() {
-    // connection.query('', function(err, data){
-    //     if (err) throw err;
-    //     console.log(data);
-    // });
+    inquirer.prompt([
+    {
+        type: 'input',
+        message: 'What is the new title for your employee? 1-Accountant, 2-Clerk, 3-Designer, 4-UI/UX Researcher, 5-Marketing Consultant, 6-Marketing Manager, 7-Network Admin, 8-Business Analyst',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is the LAST name of your employee?',
+        name: 'id',
+    },
+]).then(data => {
+        connection.query(`UPDATE employee SET role_id='${data.title}' WHERE last_name='${data.id}'`, function (err, data) {
+            if (err) throw err;
+            console.table(data);
+            console.log("Successfully Changed Role!")
+            askUser()
+        });
+    })
 }
